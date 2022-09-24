ffef6c0667a7ff4bb013c08a887731acc84e8846
Alluisio Silva <alluisio.silva@ipdec.org>
2022-09-24 14:44:03 -0400

    page home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -4,10 +4,12 @@ import entregador from "../../assets/img/entregador.svg";
 
 const Home: React.FC = () => {
   return (
-    <div className="container-main grid grid-nogutter col-10 col-offset-1">
+    <div className="grid grid-nogutter col-10 col-offset-1 container-main align-items-center  ">
       <div className="content-left">
-        <p className="text-faca-pedido">Faça seu pedido que entregamos para você!!!</p>
-        <p className="text-escolha">Escolha o seu pedido e em poucos minutos levaremos na sua porta</p>
+        <div className="mb-5">
+          <p className="text-faca-pedido">Faça seu pedido que entregamos para você!!!</p>
+        </div>
+        <p className="text-escolha mb-5">Escolha o seu pedido e em poucos minutos levaremos na sua porta</p>
         <Button label="FAZER PEDIDO" onClick={() => {}} />
       </div>
       <div className="content-right">
